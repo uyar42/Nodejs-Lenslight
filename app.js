9fea7d6311c419e4d54ac5c4b262c3a5c313a813
mehmet <71780605+uyar42@users.noreply.github.com>
2023-01-16 20:20:04 +0300

    MVC Yapısı

--- app.js
+++ app.js
@@ -1,34 +1,25 @@
-import express from "express"
-import dotenv from  "dotenv"
-import conn from "./db.js"
+import express from "express";
+import dotenv from "dotenv";
+import conn from "./db.js";
+import pageRoute from "./routes/pageRoute.js";
 
-dotenv.config()
+dotenv.config();
 
 //connection to the db
-conn()
+conn();
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
 //ejs template engine
-app.set("view engine","ejs")
-
+app.set("view engine", "ejs");
 
 //static files middleware
-app.use(express.static('public'))
+app.use(express.static("public"));
 
+//routes
+app.use("/", pageRoute);
 
-app.get("/",(req,res)=>{
-    res.render("index")
-})
-
-app.get("/about",(req,res)=>{
-    res.render("about")
-})
-
-
-
-app.listen(port ,()=>{
-
-    console.log(`Application running on port : ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Application running on port : ${port}`);
+});
